d52a0268f890169076573c4805abbb196c74ed14
Zach Smith <IAmSparticle@gmail.com>
2015-03-06 00:06:16 -0500

    Update & Add error messages

--- errs/parserErrs.h
+++ errs/parserErrs.h
@@ -6,9 +6,10 @@
 #define	ERR_PARSE_LBL_EMPTY		"\':\' Encountered without preceding label"
 #define ERR_PARSE_CMD_LONG		"Command too long"
 #define	ERR_PARSE_LBL_LETTR		"Labels must start with a letter"
-#define ERR_PARSE_LBL_BAD_CHAR	"Invalid characters found in label"
+#define ERR_PARSE_LBL_BAD_CHAR	"Invalid characters found in label: \" \""
 #define ERR_PARSE_LBL_CMD_COLL	"Command found where expecting a label"
-#define ERR_PARSE_ARG_CNT		"Wrong number or arguments found"
+#define ERR_PARSE_ARG_CNT		"Wrong number of arguments found"
+#define ERR_PARSE_CMD_NOT_FOUND	"Command does not exist"
 
 #define ERR_LBL_MULT_DEF		"Multiply-defined label found"
 #define ERR_LBL_UNKWN_TYPE		"Internal Error: unknown label type encountered"
